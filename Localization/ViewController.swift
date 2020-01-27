c8c9bfd0f65ff404a9276bc4f6e01763dfd2a8d0
sana <sana@gmail.com>
2020-01-27 14:39:49 +0500

    english to swedish localization is completed

--- Localization/ViewController.swift
+++ Localization/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var segment: UISegmentedControl!
+    
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var lastName: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func segmentAction(_ sender: Any) {
+        
+        if segment.selectedSegmentIndex == 0 {  // english
+            
+            self.changeLang(lang: "en")
+            
+        }else{ // swedish
+            self.changeLang(lang: "sv")
+        }
+    }
+    
+    func changeLang( lang : String){
+        firstName.text = "FirstNameKey".localizableString(loc: lang)
+        lastName.text = "LastNameKey".localizableString(loc: lang)
+    }
+    
 
+}
 
+extension String{
+    
+    func localizableString(loc : String) -> String{
+        
+        let path = Bundle.main.path(forResource: loc, ofType: "lproj")
+        let bundle = Bundle(path: path!)
+        
+        return NSLocalizedString(self, tableName: nil, bundle: bundle!, value: "", comment: "")
+    }
 }
 
