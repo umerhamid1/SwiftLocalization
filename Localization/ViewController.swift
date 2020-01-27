41d45b5f2e920680483cf2ea8c00952a0e5244b9
sana <sana@gmail.com>
2020-01-27 12:50:30 +0500

    Initial Commit

---
+++ Localization/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Localization
+//
+//  Created by umer hamid on 1/27/20.
+//  Copyright © 2020 umer hamid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
